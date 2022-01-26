3fcb2be56b556de19064815bd7d09d979669f29f
David Morales Luis <dmoralesluis@danielcastelao.org>
2022-01-26 14:27:39 +0100

    first commit

---
+++ src/com/GrupoCod/Main.java
@@ -0,0 +1,11 @@
+package com.GrupoCod;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("I'am the Boss");
+
+        System.out.println("first commit");
+    }
+}
